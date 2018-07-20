59c461479382ea0b0637f975e2e381a56c3d9027
Nick iloba <nick.iloba@mullenlowe.com>
2018-07-20 14:15:43 -0400

    added new files and changed names

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log(this)
+let bio = {
+    name: 'nick',
+    job: 'web-developer',
+    full: function() {
+        console.log(this.name, this.job)
+    }
+}
+bio.full()
+
+const circle = {}
+
+// Factory function
+function Bio(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log(radius)
+        }
+    }
+}
+
+console.log(Bio(5).radius)
+
+
+// constructor function
+function Bioo (radius) {
+    this.radius = radius
+}
+
