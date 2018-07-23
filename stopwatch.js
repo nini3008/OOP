38ca35efc19d9e32c22eaabdd46d83ba0651a19f
Nick iloba <nick.iloba@mullenlowe.com>
2018-07-23 17:25:00 -0400

    new fixes added to stop watch

--- stopwatch.js
+++ stopwatch.js
@@ -1,29 +1,48 @@
 function Watch() {
-    this.duration = 0;
+    let startTime, stopTime, running, duration = 0;
 
-    increment = function (duration) {
-        return duration + 1
+    this.interval = function () {
+        setInterval(function() {
+            document.getElementById('number').innerHTML = duration += 1
+        }, 1000)
     }
 
-
     this.start = function () {
-      console.log('clock started')
-      increment(this.duration)
-      console.log(this.duration)
-      setInterval(function() {
-        document.getElementById('number').innerHTML = this.duration
-        console.log(this.duration)
-      }.bind(this), 1000)
+        if(running) {
+            throw new Error('Stopwatch has already started')
+        } else {
+            running = true;
+            this.interval()
+        }
+
+        startTime = new Date();
     };
 
-    this.stop = function () {
 
+    this.stop = function () {
+        if(!running) {
+            throw new Error('Stopwatch has already been stopped')
+        } else {
+            running = false;
+            document.getElementById('number').innerHTML = 0
+        }
+        stopTime = new Date();
+        const seconds = (stopTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
     };
 
     this.reset = function () {
-        console.log('reset')
+        startTime = null;
+        stopTime = null;
+        running = 0;
+        duration = 0;
     };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            return duration;
+        }
+    })
 }
 
 const sw = new Watch()
-// sw.start()
